1f0ac2ed0a1cf4ba2c72bb5f61e465839eea54cb
EmersonGalerians <Emersonjesuscandido@hotmail.com>
2025-07-28 21:34:02 -0300

    Deleted past

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+
+new Swiper('.card-wrapper', {
+    // Optional parameters
+
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+
+function carro(){
+  window.open("http://Curriculo/src/paginas/devcar-main/index.html", "_blank");
+}
+function jogo(){
+  window.open("http://Curriculo/src/paginas/devcar-main/index.html", "_blank");
+}
